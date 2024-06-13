0787d8e6062128588c30a3469e900b7c517670fc
sirkaue <kauekdslopes@gmail.com>
2024-06-13 16:52:34 -0300

    Teste - setIncomeShouldThrowIllegalArgumentExceptionWhenInvalidatedData

--- src/test/java/entity/FinancingTests.java
+++ src/test/java/entity/FinancingTests.java
@@ -73,4 +73,18 @@ public class FinancingTests {
         financing.setIncome(5000.0);
         Assertions.assertEquals(5000.00, financing.getIncome());
     }
+
+    @Test
+    public void setIncomeShouldThrowIllegalArgumentExceptionWhenInvalidatedData() {
+
+        double totalAmount = 100000.0;
+        double income = 2000.0;
+        int months = 80;
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Financing financing = FinancingFactory.createFinancing(totalAmount, income, months);
+
+            financing.setIncome(1900.0);
+        });
+    }
 }
